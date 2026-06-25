7ea67289d7e7f0dff6e95ef0e1985b9cea8a2f97
misuzu-moder <misuzudog99@gmail.com>
2026-06-25 18:53:20 +0900

    feat: 投稿作成画面とルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
+
+Route::get('/post/create', [PostController::class, 'create']);
 
 require __DIR__.'/settings.php';
